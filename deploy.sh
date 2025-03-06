fa5beae5bf5cbe4a5fbb0f658c0a5477f5fdebc6
Daniel Müller <daniel.mueller@surnet.ch>
2025-03-06 21:56:29 +0100

    Deployment

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+export AWS_PROFILE=dev
+
+aws cloudformation list-type-versions --type RESOURCE --type-name Surnet::ParameterStore::Secret | jq '.TypeVersionSummaries[] | .Arn' | xargs -n1 aws cloudformation --profile dev deregister-type --arn
+aws cloudformation deregister-type --type RESOURCE --type-name Surnet::ParameterStore::Secret || true
+cfn generate
+cfn submit --set-default
