cb75887d70124c220a8107e63e4c75956a6c7ca2
Daniel Müller <daniel.mueller@surnet.ch>
2025-03-07 11:19:23 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-aws cloudformation list-type-versions --type RESOURCE --type-name Surnet::ParameterStore::Secret | jq '.TypeVersionSummaries[] | .Arn' | xargs -n1 aws cloudformation --profile dev deregister-type --arn
+aws cloudformation list-type-versions --type RESOURCE --type-name Surnet::ParameterStore::Secret | jq '.TypeVersionSummaries[] | .Arn' | xargs -n1 aws cloudformation deregister-type --arn
 aws cloudformation deregister-type --type RESOURCE --type-name Surnet::ParameterStore::Secret || true
 cfn generate
 cfn submit --set-default
